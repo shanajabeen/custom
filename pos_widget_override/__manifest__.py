4cf40bff2e661cb4bd73183c3953138235487edd
shanajabeen <shanajabeenok@gmail.com>
2021-01-15 16:52:15 +0530

    $

--- pos_widget_override/__manifest__.py
+++ pos_widget_override/__manifest__.py
@@ -20,13 +20,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','point_of_sale',],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        
+        # 'views/view1.xml'
+
       
     ],
     # only loaded in demonstration mode
--- pos_widget_override/models/models.py
+++ pos_widget_override/models/models.py
@@ -1,18 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import fields, models, api
 
 
-# class pos_widget_override(models.Model):
-#     _name = 'pos_widget_override.pos_widget_override'
-#     _description = 'pos_widget_override.pos_widget_override'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
